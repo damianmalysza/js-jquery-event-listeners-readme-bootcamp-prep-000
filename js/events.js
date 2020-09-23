e586ebde3ba2777fbc4119e8fd9ba3280a038bbe
Damian Malysza <damian.malysza@gmail.com>
2020-09-23 01:03:16 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -10,7 +10,9 @@ function frameIt(){
 
 function pressIt(){
   $("input").on("keydown",function(key){
-    if(key.which===)
+    if(key.which===71){
+      alert("You pressed the G key!")
+    }
   })
 }
 
